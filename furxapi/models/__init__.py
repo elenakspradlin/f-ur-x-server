615deabbd7f482499568caf21e682378de657f90
Elena Spradlin <ekspradlin@gmail.com>
2023-09-09 23:25:18 -0400

    Login/Register work in progress

--- furxapi/models/__init__.py
+++ furxapi/models/__init__.py
@@ -3,4 +3,4 @@ from .blog_model import Blog
 from .to_do_list_model import ToDoList
 from .user_items_model import UserItems
 from .registry_item_model import RegistryItem
-from .furx_profile_model import FURXProfileInformation
+from .furx_user_profile_model import FURXUserProfileInformation
