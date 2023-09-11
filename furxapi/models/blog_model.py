4af54344f903a3ed5f73e44c480de84299a36adb
Elena Spradlin <ekspradlin@gmail.com>
2023-09-11 13:17:07 -0400

    added fixtures

--- furxapi/models/blog_model.py
+++ furxapi/models/blog_model.py
@@ -8,4 +8,5 @@ class Blog(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     blog_post = models.CharField(max_length=5000)
     better_than_yesterday = models.BooleanField(default=False)
+    # does this one need to be a Foreign Key field instead?
     feeling = models.OneToOneField(Feeling, on_delete=models.CASCADE)
